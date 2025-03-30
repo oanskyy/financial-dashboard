1ed0ef36e9a3f91bcdd881385a3869a73895e638
oanskyy <oana.anghelachi@gmail.com>
2025-03-30 20:02:11 +0100

    feat: handling all errors with erros.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-
 	await sql`DELETE FROM invoices WHERE id = ${id}`
 	revalidatePath("/dashboard/invoices")
 }
